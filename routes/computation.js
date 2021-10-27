99d9ff9450626a9a0cb07aede0b999f41a3de6ff
Satishgundlapally <89225850+Satishgundlapally@users.noreply.github.com>
2021-10-27 11:01:22 -0500

    Update computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var value=Math.random()*10;
-console.log(value);
+var value=Math.round(Math.random()*100);
+
 var s= Math.log2(value)  ;
+var t=Math.cosh(value) 
+var h=Math.floor(value)
  
 //var g= Math.cosh(value) ;
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
  // res.send(`[ Math.log2() ] applied to [${value}] is [${s}][Math.cosh() ] applied to [${value}] is [${g}]`);
- res.send('the function [Math.log2() applied to] ['+ value+'] is ['+s+']');
+ res.send('the function [Math.log2() applied to] ['+ value+'] is ['+s+']\nthe function [Math.cosh() applied to] ['+ value+'] is ['+t+']\nthe function [Math.log2() applied to] ['+ value+'] is ['+h+']');
 
 });
 
