680bcae4c90d3d95e765db427def47618a7c319d
Satishgundlapally <89225850+Satishgundlapally@users.noreply.github.com>
2021-10-27 10:13:43 -0500

    added bonus

--- routes/computation.js
+++ routes/computation.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 var value=Math.random()*10;
 console.log(value);
-var s= Math.log2()  ;
+var s= Math.log2(value)  ;
  
-var g= Math.cosh() ;
+var g= Math.cosh(value) ;
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
