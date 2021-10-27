02e752f72eb44cfc0d4f4640da27383998c538ae
Satishgundlapally <89225850+Satishgundlapally@users.noreply.github.com>
2021-10-27 10:39:35 -0500

    added bonus

--- routes/computation.js
+++ routes/computation.js
@@ -4,12 +4,12 @@ var value=Math.random()*10;
 console.log(value);
 var s= Math.log2(value)  ;
  
-var g= Math.cosh(value) ;
+//var g= Math.cosh(value) ;
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  res.send(`[ Math.log2() ] applied to [${value}] is [${s}]
-[Math.cosh() ] applied to [${value}] is [${g}]`);
+ // res.send(`[ Math.log2() ] applied to [${value}] is [${s}][Math.cosh() ] applied to [${value}] is [${g}]`);
+ res.send('the function [Math.log2() applied to] ['+ value+'] is ['+s+']');
 
 });
 
